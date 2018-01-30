c81754bf0dce86dae1cb073fb1a1aae73213564a
makoto abe <abe00makoto@gmail.com>
2018-01-30 14:45:54 +0900

    i want

--- src/counter/Container.tsx
+++ src/counter/Container.tsx
@@ -1,11 +1,14 @@
 import { Counter } from './Counter';
-import { connect } from 'react-redux';
+import * as ReactRedux from 'react-redux';
 import { Dispatch } from 'redux';
 import { decrementAmount, incrementAmount } from './module';
 import { ReduxAction, ReduxState } from '../store';
 
-export class ActionDispatcher {
-  constructor(private dispatch: (action: ReduxAction) => void, rootState: ReduxState) { }
+export class ActionDispatcherBase {
+  constructor(protected dispatch: (action: ReduxAction) => void, rootState: ReduxState) { }
+}
+
+export class ActionDispatcher extends ActionDispatcherBase {
 
   public increment(amount: number) {
     this.dispatch(incrementAmount(amount));
@@ -16,11 +19,34 @@ export class ActionDispatcher {
   }
 }
 
-export default connect(
-  (state: ReduxState) => ({ state }),
-  (dispatch: Dispatch<ReduxAction>) => ({ dispatch }),
-  ({state}, {dispatch}, onwProps) => ({
-    actions: new ActionDispatcher(dispatch, state),
-    value: state.counter,
-  })
-)(Counter);
+interface MyMergeProps<T> {
+  (state: ReduxState, dispatch: Dispatch<ReduxAction>, onwProps: T): T;
+}
+
+function Myconnect<T>(actionDispatcher: typeof ActionDispatcherBase, mergeProps: MyMergeProps<T>) {
+  return ReactRedux.connect(
+    (state: ReduxState) => ({ state }),
+    (dispatch: Dispatch<ReduxAction>) => ({ dispatch }),
+    ({ state }, { dispatch }, onwProps: T) => {
+      const pp = mergeProps(state, dispatch, onwProps);
+      return (
+        Object.assign({}, {
+          actions: new actionDispatcher(dispatch, state)
+        }, pp)
+      );
+    });
+}
+
+// export default ReactRedux.connect(
+//   (state: ReduxState) => ({ state }),
+//   (dispatch: Dispatch<ReduxAction>) => ({ dispatch }),
+//   ({ state }, { dispatch }, onwProps) => ({
+//     actions: new ActionDispatcher(dispatch, state),
+//     value: state.counter,
+//   })
+// )(Counter);
+
+export default Myconnect(
+  ActionDispatcher, (state, dispatch, ownprops) => ({
+  value: state.counter,
+}))(Counter);
